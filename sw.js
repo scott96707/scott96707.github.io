42091e23e51b53ecd9bd1d1c0161717649e37296
scott96707 <scott96707@gmail.com>
2019-01-29 10:40:30 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1fcb346d94b830b180cd.js"
+    "url": "webpack-runtime-b4c39aa580332b9e6d54.js"
   },
   {
     "url": "app-bdf049ef4ac7d22cbc09.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8120d827a5d7c8ec12835a6b4e18a05e"
+    "revision": "d8bfda3e99544537bd67b562b1d4b88e"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
