56603bca83f8fe6a1bdb5e20bba98a4d1a063e81
scott96707 <scott96707@gmail.com>
2019-04-24 13:03:26 -0600

    Cleaned up header section

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => (
   <header
     style={{
       background: `rebeccapurple`,
-      height: `4vw`,
+      height: `5vw`,
       width: `100%`,
       margin: `0`,
       padding: `0`,
@@ -21,57 +21,27 @@ const Header = () => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <a
-          href="#home"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <a href="#home" style={{ color: `white`, textDecoration: `none` }} >
           Home
         </a>
       </h1>
       <h1 style={{ margin: 0 }}>
-        <a
-          href="#about"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <a href="#about" style={{ color: `white`, textDecoration: `none` }} >
           About
         </a>
       </h1>
       <h1 style={{ margin: 0 }}>
-        <a
-          href="#projects"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <a href="#projects" style={{ color: `white`, textDecoration: `none` }} >
           Projects
         </a>
       </h1>
       <h1 style={{ margin: 0 }}>
-        <a
-          href="#contactme"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <a href="#contactme" style={{ color: `white`, textDecoration: `none` }} >
           Contact Me
         </a>
       </h1>
       <h1 style={{ margin: 0 }}>
-          <a
-            href="http://github.com/scott96707"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
+          <a href="http://github.com/scott96707" style={{ color: `white`, textDecoration: `none` }} >
             Github
           </a>
       </h1>
