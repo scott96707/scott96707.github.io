cce4460093bfac130c701659b994e52c78c4cea7
scott96707 <scott96707@gmail.com>
2019-04-24 14:22:20 -0600

    Fixing a lot of formatting. Adding @media queries

--- src/components/header.js
+++ src/components/header.js
@@ -1,52 +1,43 @@
 import { a } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./style.css"
 
 const Header = () => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      height: `5vw`,
-      width: `100%`,
-      margin: `0`,
-      padding: `0`,
-    }}
-  >
-    <div
+  <nav>
+    <ul
+      className="navbar__list"
       style={{
-        display: `inline-flex`,
-        alignContent: `center`,
-        justifyContent: `space-around`,
-        width: `1vw`,
+        
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <li style={{ margin: 0 }}>
         <a href="#home" style={{ color: `white`, textDecoration: `none` }} >
           Home
         </a>
-      </h1>
-      <h1 style={{ margin: 0 }}>
+      </li>
+      <li style={{ margin: 0 }}>
         <a href="#about" style={{ color: `white`, textDecoration: `none` }} >
           About
         </a>
-      </h1>
-      <h1 style={{ margin: 0 }}>
+      </li>
+      <li style={{ margin: 0 }}>
         <a href="#projects" style={{ color: `white`, textDecoration: `none` }} >
           Projects
         </a>
-      </h1>
-      <h1 style={{ margin: 0 }}>
+      </li>
+      <li style={{ margin: 0 }}>
         <a href="#contactme" style={{ color: `white`, textDecoration: `none` }} >
           Contact Me
         </a>
-      </h1>
-      <h1 style={{ margin: 0 }}>
+      </li>
+      <li style={{ margin: 0 }}>
           <a href="http://github.com/scott96707" style={{ color: `white`, textDecoration: `none` }} >
             Github
           </a>
-      </h1>
-    </div>
-  </header>
+      </li>
+    </ul>
+  </nav>
 )
 
 Header.propTypes = {
--- src/components/intro.js
+++ src/components/intro.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Intro = () => {
     return (
-        <div>
+        <div className="intro__section" >
             <h1>Scott Green</h1>
             <h2>Web Developer</h2>
         </div>
